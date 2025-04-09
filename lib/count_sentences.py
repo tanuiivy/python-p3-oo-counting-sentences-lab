2878da58fc48eed521094ab09e10aeca691b3b83
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-04-09 20:34:17 +0300

    Task complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self._value = ""
+    self.value = value 
+  
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, new_value):
+    if isinstance(new_value, str):
+      self._value = new_value
+    else:
+        print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+
+  def count_sentences(self):
+        temp_value = self.value  
+        for punc in ["!", "?"]:
+            temp_value = temp_value.replace(punc, ".")
+        sentences = [s.strip() for s in temp_value.split(".") if s.strip()]
+        return len(sentences)
+
